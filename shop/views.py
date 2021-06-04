57eb23391890072ea99c02b8a6d69b34fae8ae2f
Kusum-Maurya10 <kusummaurya256@gmail.com>
2021-06-04 11:08:01 +0530

    initial commit

--- shop/views.py
+++ shop/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from PayTm import Checksum
 # Create your views here.
 from django.http import HttpResponse
-MERCHANT_KEY = 'fLNj2cUOv92s#qD3'
+MERCHANT_KEY = '#zoDj6nZ!0jDoYJw'
 
 def index(request):
     allProds = []
@@ -113,7 +113,7 @@ def checkout(request):
         # Request paytm to transfer the amount to your account after payment by user
         param_dict = {
 
-                'MID': 'uQjiPX52832375711349',
+                'MID': 'rdfbHW85732181982017',
                 'ORDER_ID': str(order.order_id),
                 'TXN_AMOUNT': str(amount),
                 'CUST_ID': email,
